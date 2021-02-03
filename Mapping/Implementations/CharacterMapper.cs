effc61c6c1b1221550db7b28a8bbc5cee26ced51
Jacob Lavarnway <lavarnway932@gmail.com>
2021-02-03 17:51:59 -0500

    Added implementation for mapping the Character model

---
+++ Mapping/Implementations/CharacterMapper.cs
@@ -0,0 +1,65 @@
+﻿using DnDProject.Backend.Mapping.Interfaces;
+using DnDProject.Entities.Character.DataModels;
+using DnDProject.Entities.Character.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace DnDProject.Backend.Mapping.Implementations
+{
+    public class CharacterMapper : ICharacterMapper
+    {
+        //Create
+        public Character mapCharacterVMToNewEntity(CharacterVM vm)
+        {
+            Character character = new Character();
+
+            //Map all items from the provided VM to a new Character entity. Does NOT map any source fields if the source field is null.
+            var CharacterVM_To_New_Character = new MapperConfiguration(
+                cfg => cfg.CreateMap<CharacterVM, Character>()
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null))
+                );
+            var mapper = CharacterVM_To_New_Character.CreateMapper();
+
+            mapper.Map<CharacterVM, Character>(vm, character);
+
+            return character;
+        }
+
+
+        //Read
+        public CharacterVM mapCharacterToCharacterVM(Character m)
+        {
+            CharacterVM vm = new CharacterVM();
+
+            var Character_To_CharacterVM = new MapperConfiguration(
+                cfg => cfg.CreateMap<Character, CharacterVM>()
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null))
+                );
+
+            var mapper = Character_To_CharacterVM.CreateMapper();
+
+            mapper.Map<Character, CharacterVM>(m, vm);
+
+            return vm;
+        }
+
+
+        //Update
+        public void mapCharacterVMToExistingEntity(CharacterVM vm, Character m)
+        {
+            //Map all items from CharacterVM to the an existing DB entity for a character's Character. Does NOT map any source fields if the source field is null.
+            var CharacterVM_To_Entity = new MapperConfiguration(
+                cfg => cfg.CreateMap<CharacterVM, Character>()
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null))
+            );
+            var mapper = CharacterVM_To_Entity.CreateMapper();
+
+            mapper.Map<CharacterVM, Character>(vm, m);
+
+        }
+    }
+}
---
+++ Mapping/Interfaces/ICharacterMapper.cs
@@ -0,0 +1,24 @@
+﻿using DnDProject.Entities.Character.DataModels;
+using DnDProject.Entities.Character.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDProject.Backend.Mapping.Interfaces
+{
+    public interface ICharacterMapper
+    {
+
+        //Create
+        Character mapCharacterVMToNewEntity(CharacterVM vm);
+
+
+        //Read
+        CharacterVM mapCharacterToCharacterVM(Character m);
+
+        //Update
+        void mapCharacterVMToExistingEntity(CharacterVM vm, Character m);
+    }
+}

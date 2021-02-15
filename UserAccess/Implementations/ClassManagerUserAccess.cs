490f156f2404de913fae7529e32c52ca9c8275d7
Jacob Lavarnway <lavarnway932@gmail.com>
2021-02-15 17:37:51 -0500

    implemented ClassManagerUserAccess

---
+++ UserAccess/Implementations/ClassManagerUserAccess.cs
@@ -0,0 +1,76 @@
+﻿using DnDProject.Backend.Unit_Of_Work.Interfaces;
+using DnDProject.Backend.UserAccess.Interfaces;
+using DnDProject.Entities.Class.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDProject.Backend.UserAccess.Implementations
+{
+    public class ClassManagerUserAccess : BaseUserAccess, IClassManagerUserAccess
+    {
+        IUnitOfWork _worker;
+
+        public void AddPlayableClass(PlayableClass playableClass)
+        {
+            _worker.Classes.Add(playableClass);
+        }
+        public void RemovePlayableClass(PlayableClass toBeRemoved)
+        {
+            _worker.Classes.Remove(toBeRemoved);
+        }
+        public void AddAbility(ClassAbility ability)
+        {
+            _worker.ClassAbilities.Add(ability);
+        }
+        public void AddAbilities(IEnumerable<ClassAbility> abilities)
+        {
+            _worker.ClassAbilities.AddRange(abilities);
+        }
+        
+        public IEnumerable<ClassAbility> GetAllClassAbilities()
+        {
+            return _worker.ClassAbilities.GetAll();
+        }
+        public void RemoveClassAbility(ClassAbility toBeRemoved)
+        {
+            _worker.ClassAbilities.Remove(toBeRemoved);
+        }
+
+
+        public void AddSubclass(Subclass subclass)
+        {
+            _worker.Subclasses.Add(subclass);
+        }
+        public void AddSubclasses(IEnumerable<Subclass> subclasses)
+        {
+            _worker.Subclasses.AddRange(subclasses);
+        }
+        public void RemoveSubclass(Subclass toBeRemoved)
+        {
+            _worker.Subclasses.Remove(toBeRemoved);
+        }
+
+
+
+        public void AddSubclassAbility(SubclassAbility subclassAbility)
+        {
+            _worker.SubclassAbilities.Add(subclassAbility);
+        }
+        public void AddSubclassAbilities(IEnumerable<SubclassAbility> subclassAbilities)
+        {
+            _worker.SubclassAbilities.AddRange(subclassAbilities);
+        }
+        public void RemoveSubclassAbility(SubclassAbility toBeRemoved)
+        {
+            _worker.SubclassAbilities.Remove(toBeRemoved);
+        }
+
+        public ClassManagerUserAccess(IUnitOfWork worker) : base(worker)
+        {
+            _worker = worker;
+        }
+    }
+}
---
+++ UserAccess/Interfaces/IClassManagerUserAccess.cs
@@ -0,0 +1,30 @@
+﻿using DnDProject.Entities.Class.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDProject.Backend.UserAccess.Interfaces
+{
+    public interface IClassManagerUserAccess : IBaseUserAccess
+    {
+        void AddPlayableClass(PlayableClass playableClass);
+        void RemovePlayableClass(PlayableClass toBeRemoved);
+        void AddAbility(ClassAbility ability);
+        void AddAbilities(IEnumerable<ClassAbility> abilities);
+        IEnumerable<ClassAbility> GetAllClassAbilities();
+        void RemoveClassAbility(ClassAbility toBeRemoved);
+
+        void AddSubclass(Subclass subclass);
+        void AddSubclasses(IEnumerable<Subclass> subclasses);
+        void RemoveSubclass(Subclass subclass);
+
+
+        void AddSubclassAbility(SubclassAbility subclassAbility);
+        void AddSubclassAbilities(IEnumerable<SubclassAbility> subclassAbilities);
+        void RemoveSubclassAbility(SubclassAbility toBeRemoved);
+
+
+    }
+}
